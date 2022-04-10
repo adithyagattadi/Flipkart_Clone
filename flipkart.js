0c05c3efff1a5e366c36ca649ab0757ffad845d8
vp-1234ms <vaibhav444pawar555@gmail.com>
2022-04-10 21:49:16 +0530

    initial

---
+++ flipkart.js
@@ -0,0 +1,25 @@
+
+        let i = document.getElementById("arrow");
+        function clicked(){
+            
+            if (image.src="offer2 (1).jpg"){
+                image.src="offer3.jpeg";
+                console.log("Hiii");
+            }
+            else if (image.src="offer3.jpeg"){
+                image.src="offer4.jpeg";
+                console.log("Hiii");
+            }
+            else if (image.src="offer4.jpeg"){
+                image.src="offer5.jpeg";
+                console.log("Hiii");
+            }
+            else if (image.src="offer5.jpeg"){
+                image.src="offer6.jpeg";
+                console.log("Hiii");
+            }
+            else{
+                image.src="offer2 (1).jpg";
+                console.log("Hiii");
+            }
+        }  
---
+++ hi.js
@@ -0,0 +1,37 @@
+// function clicked(){
+//     console.log("the button was clicked")
+// }
+// window.onload = function(){
+//     console.log("The page was loaded");
+// }
+// firstcontainer.addEventListener("click",function(){
+//     console.log("You clicked")
+// })
+// firstcontainer.addEventListener("mouseover",function(){
+//     console.log("mouse over")
+// })
+// firstcontainer.addEventListener("mouseout",function(){
+//     console.log("mouse out")
+// })
+// firstcontainer.addEventListener("mouseup",function(){
+//     document.querySelectorAll('.container')[1].innerHTML="<b>We have clicked on this</b>"
+// })
+// let prev=document.querySelectorAll('.container')[1].innerHTML;
+// firstcontainer.addEventListener("mousedown",function(){
+//     document.querySelectorAll('.container')[1].innerHTML=prev;
+// })
+// firstcontainer.addEventListener("mouseover",function(){
+//     document.querySelectorAll('.container')[1].c="<b>We have clicked on this</b>"
+// })
+// let coloree=document.querySelectorAll('.container')[1].;
+// firstcontainer.addEventListener("mouseout",function(){
+//     document.querySelectorAll('.container')[1].innerHTML=coloree;
+// })
+// callme=() =>{
+//     document.querySelectorAll(".container")[1].innerHTML="<b>lorem crcrxsxje njcnrcx djxnjx </b>"
+// }
+// setTimeout(callme,2000);
+setInterval(hi,2000);
+hi=() =>{
+    
+}
